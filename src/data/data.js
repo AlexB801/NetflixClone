cba7341ab112d87d8e3fb42456ebce15fe391e60
Alex Bennett <benedictus.ab@gmail.com>
2018-04-12 13:08:31 -0600

    initial commit

---
+++ src/data/data.js
@@ -0,0 +1,115 @@
+const HOUSE_OF_CARDS = require('../../assets/images/house-of-cards.jpeg');
+const STRANGER_THINGS = require('../../assets/images/stranger-things.jpeg');
+const ORANGE_IS_THE_NEW_BLACK = require('../../assets/images/orange-is-the-new-black.jpeg');
+const THE_OFFICE = require('../../assets/images/The-Office.jpg');
+const LOST = require('../../assets/images/Lost.jpg');
+
+const SHOWS = [
+  {
+    title: 'Lost',
+    summary: 'The show contains elements of supernatural and science fiction, and follows the survivors of a commercial jet airliner crash, flying between Sydney and Los Angeles, California, on a mysterious tropical island somewhere in the South Pacific Ocean.',
+    starring: 'Matthew Fox, Jorge Garcia, Evangeline Lilly, ...',
+    creator: 'Jeffrey Lieber, J.J. Abrams and Damon Lindelof',
+    image: LOST,
+  },
+  {
+    title: 'House Of Cards',
+    summary: "Set in present-day Washington, D.C., House of Cards is the story of Frank Underwood (Kevin Spacey), a Democrat from South Carolina's 5th congressional district and the House Majority Whip, who, after getting passed over for appointment as Secretary of State, decides to exact his revenge on those who betrayed him.",
+    starring: 'Kevin Spacey, Robin Wright, ...',
+    creator: 'Beau Willimon',
+    image: HOUSE_OF_CARDS,
+  },
+  {
+    title: 'The Office',
+    summary: 'The series depicts the everyday lives of office employees in the Scranton, Pennsylvania, branch of the fictional Dunder Mifflin Paper Company.',
+    starring: 'Steve Carrell, Rainn Wilson, John Krasinski, ...',
+    creator: 'Greg Daniels',
+    image: THE_OFFICE,
+  },
+  {
+    title: 'Orange Is The New Black',
+    summary: "The series, based on Piper Kerman's memoir Orange Is the New Black: My Year in a Women's Prison (2010), follows Piper Chapman (Taylor Schilling), a happily engaged New Yorker who is sent to a women's federal prison for transporting a suitcase full of drug money across international borders, 10 years prior, for her girlfriend at the time, Alex Vause (Laura Prepon).",
+    starring: 'Taylor Schilling, Laura Prepon, Jason Biggs, ...',
+    creator: 'Jenji Kohan',
+    image: ORANGE_IS_THE_NEW_BLACK,
+  },
+  {
+    title: 'Stranger Things',
+    summary: 'When a young boy vanishes, a small town uncovers a mystery involving a secret experiment, terrify supernatural forces, and one strange little girl',
+    starring: 'Winona Ryder, David Harbour, Mathew Modine, ...',
+    creator: 'The Duffer Brothers',
+    image: STRANGER_THINGS,
+  },
+  {
+    title: 'Lost',
+    summary: 'The show contains elements of supernatural and science fiction, and follows the survivors of a commercial jet airliner crash, flying between Sydney and Los Angeles, California, on a mysterious tropical island somewhere in the South Pacific Ocean.',
+    starring: 'Matthew Fox, Jorge Garcia, Evangeline Lilly, ...',
+    creator: 'Jeffrey Lieber, J.J. Abrams and Damon Lindelof',
+    image: LOST,
+  },
+  {
+    title: 'House Of Cards',
+    summary: "Set in present-day Washington, D.C., House of Cards is the story of Frank Underwood (Kevin Spacey), a Democrat from South Carolina's 5th congressional district and the House Majority Whip, who, after getting passed over for appointment as Secretary of State, decides to exact his revenge on those who betrayed him.",
+    starring: 'Kevin Spacey, Robin Wright, ...',
+    creator: 'Beau Willimon',
+    image: HOUSE_OF_CARDS,
+  },
+  {
+    title: 'The Office',
+    summary: 'The series depicts the everyday lives of office employees in the Scranton, Pennsylvania, branch of the fictional Dunder Mifflin Paper Company.',
+    starring: 'Steve Carrell, Rainn Wilson, John Krasinski, ...',
+    creator: 'Greg Daniels',
+    image: THE_OFFICE,
+  },
+  {
+    title: 'Orange Is The New Black',
+    summary: "The series, based on Piper Kerman's memoir Orange Is the New Black: My Year in a Women's Prison (2010), follows Piper Chapman (Taylor Schilling), a happily engaged New Yorker who is sent to a women's federal prison for transporting a suitcase full of drug money across international borders, 10 years prior, for her girlfriend at the time, Alex Vause (Laura Prepon).",
+    starring: 'Taylor Schilling, Laura Prepon, Jason Biggs, ...',
+    creator: 'Jenji Kohan',
+    image: ORANGE_IS_THE_NEW_BLACK,
+  },
+  {
+    title: 'StrangerThings',
+    summary: 'When a young boy vanishes, a small town uncovers a mystery involving a secret experiment, terrify supernatural forces, and one strange little girl',
+    starring: 'Winona Ryder, David Harbour, Mathew Modine, ...',
+    creator: 'The Duffer Brothers',
+    image: STRANGER_THINGS,
+  },
+  {
+    title: 'Lost',
+    summary: 'The show contains elements of supernatural and science fiction, and follows the survivors of a commercial jet airliner crash, flying between Sydney and Los Angeles, California, on a mysterious tropical island somewhere in the South Pacific Ocean.',
+    starring: 'Matthew Fox, Jorge Garcia, Evangeline Lilly, ...',
+    creator: 'Jeffrey Lieber, J.J. Abrams and Damon Lindelof',
+    image: LOST,
+  },
+  {
+    title: 'House Of Cards',
+    summary: "Set in present-day Washington, D.C., House of Cards is the story of Frank Underwood (Kevin Spacey), a Democrat from South Carolina's 5th congressional district and the House Majority Whip, who, after getting passed over for appointment as Secretary of State, decides to exact his revenge on those who betrayed him.",
+    starring: 'Kevin Spacey, Robin Wright, ...',
+    creator: 'Beau Willimon',
+    image: HOUSE_OF_CARDS,
+  },
+  {
+    title: 'The Office',
+    summary: 'The series depicts the everyday lives of office employees in the Scranton, Pennsylvania, branch of the fictional Dunder Mifflin Paper Company.',
+    starring: 'Steve Carrell, Rainn Wilson, John Krasinski, ...',
+    creator: 'Greg Daniels',
+    image: THE_OFFICE,
+  },
+  {
+    title: 'Orange Is The New Black',
+    summary: "The series, based on Piper Kerman's memoir Orange Is the New Black: My Year in a Women's Prison (2010), follows Piper Chapman (Taylor Schilling), a happily engaged New Yorker who is sent to a women's federal prison for transporting a suitcase full of drug money across international borders, 10 years prior, for her girlfriend at the time, Alex Vause (Laura Prepon).",
+    starring: 'Taylor Schilling, Laura Prepon, Jason Biggs, ...',
+    creator: 'Jenji Kohan',
+    image: ORANGE_IS_THE_NEW_BLACK,
+  },
+  {
+    title: 'Stranger Things',
+    summary: 'When a young boy vanishes, a small town uncovers a mystery involving a secret experiment, terrify supernatural forces, and one strange little girl',
+    starring: 'Winona Ryder, David Harbour, Mathew Modine, ...',
+    creator: 'The Duffer Brothers',
+    image: STRANGER_THINGS,
+  },
+];
+
+export default SHOWS;
